cf66b69ef19432606e71a928406e6087d1f9b37a
Tamas Varadi <varadi.thomas@gmail.com>
2022-10-28 19:53:06 +0200

    main button added with click event

---
+++ web-frontend/src/components/MainButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="$emit('buttonClicked')">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MainButton',
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    width: 450px;
+    height: 55px;
+    background: #D9D9D9;
+    border: 2px solid #000000;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  span {
+    font-size: 20px;
+  }
+</style>
