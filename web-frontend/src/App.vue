9c452c97a8df5e314cf19355aa4c54152327c332
Tamas Varadi <varadi.thomas@gmail.com>
2022-10-29 10:56:22 +0200

    added project files

---
+++ web-frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link to="/">Go to Home</router-link>
+  <div>separator</div>
+  <router-link to="/signup">Go to Signup</router-link>
+  <div>separator</div>
+  <router-link to="/login">Go to Login</router-link>
+
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
