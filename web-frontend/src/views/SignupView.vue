9d77d230170bdb3f4c071a2a2b47964382a0064f
Tamas Varadi <varadi.thomas@gmail.com>
2022-10-31 21:09:33 +0100

    signup view sitebuild

--- web-frontend/src/views/SignupView.vue
+++ web-frontend/src/views/SignupView.vue
@@ -1,22 +1,66 @@
 <template>
-  <div>
-    <MainInput label="Email" input-type="password"/>
+  <div class="signup-view-wrapper">
+    <div class="signup-view mt-5">
+      <MainInput class="mt-2" label="Email" input-type="email" @input-change="onEmailInputChange"/>
+      <MainInput class="mt-2" label="Password" input-type="password" @input-change="onPasswordInputChange"/>
+      <MainInput class="mt-2" label="Confirm password" input-type="password" @input-change="onConfirmationPasswordInputChange"/>
+      <MainButton class="mt-4" text="Sign up" @button-click="onSignupButtonClick"/>
+    </div>
   </div>
 </template>
 
 <script>
 import MainInput from "@/components/MainInput";
+import MainButton from "@/components/MainButton";
 
 export default {
   name: "SignupView",
+
+  data: function () {
+    return {
+      email: '',
+      password: '',
+      confirmationPassword: '',
+    }
+  },
+
+  methods: {
+    onEmailInputChange(emailInput) {
+      this.email = emailInput;
+    },
+
+    onPasswordInputChange(passwordInput) {
+      this.password = passwordInput
+    },
+
+    onConfirmationPasswordInputChange(confirmationPasswordInput) {
+      this.confirmationPassword = confirmationPasswordInput;
+    },
+
+    onSignupButtonClick() {
+      console.log(this.email);
+      console.log(this.password);
+      console.log(this.confirmationPassword);
+    }
+  },
+
   components: {
+    MainButton,
     MainInput,
   }
 }
 </script>
 
 <style scoped>
-  div {
-    background-color: aqua;
+  .signup-view-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  .signup-view {
+    width: 450px;
   }
 </style>
