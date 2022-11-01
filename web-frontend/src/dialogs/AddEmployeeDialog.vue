c5d0ba32546665dd37dbd07a762c8e1ced0a1d40
Tamas Varadi <varadi.thomas@gmail.com>
2022-11-01 16:00:15 +0100

    add employee dialog

---
+++ web-frontend/src/dialogs/AddEmployeeDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog class="dialog-wrapper">
+    <h1>Add a new employee</h1>
+    <MainInput class="mt-5 dialog-element" label="Employee's email" input-type="text" @input-change="onEmployeeEmailChange"/>
+    <MainButton class="mt-button dialog-element" text="Add employee" @button-click="$emit('addEmployee', employeeEmail)"/>
+    <MainButton class="mt-button dialog-element" text="Cancel" @button-click="$emit('closeDialog')"/>
+  </v-dialog>
+</template>
+
+<script>
+import MainButton from "@/components/MainButton";
+import MainInput from "@/components/MainInput";
+
+export default {
+  name: "AddEmployeeDialog",
+
+  components: {
+    MainInput,
+    MainButton
+  },
+
+  data: function () {
+    return {
+      employeeEmail: '',
+    }
+  },
+
+  methods: {
+    onEmployeeEmailChange(employeeEmailInput) {
+      this.employeeEmail = employeeEmailInput;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
