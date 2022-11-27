3888e138f7877865c814adb0feff24e75735be42
Tamas Varadi <varadi.thomas@gmail.com>
2022-11-28 00:32:50 +0100

    added loading spinner to request interceptor

--- web-frontend/src/App.vue
+++ web-frontend/src/App.vue
@@ -9,5 +9,29 @@
 <script>
   export default {
     name: 'App',
+
+    beforeMount() {
+      let loader;
+
+      this.axios.interceptors.request.use((request) => {
+        loader = this.$loading.show();
+
+        // const token = "dsdsadadas";
+        // const tokenValidity = "dateofvalidity";
+        //
+        // if (tokenValidity > Date.now()) {
+        //   request.headers.common.Authorization = `Bearer ${token}`;
+        // } else {
+        //
+        // }
+
+        return request;
+      });
+
+      this.axios.interceptors.response.use((response) => {
+        loader.hide();
+        return response;
+      });
+    },
   }
 </script>
--- web-frontend/src/main.js
+++ web-frontend/src/main.js
@@ -6,12 +6,16 @@ import { loadFonts } from './plugins/webfontloader'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueQRCodeComponent from 'vue-qr-generator'
+import {LoadingPlugin} from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
 
 loadFonts()
 
 createApp(App)
+    .use(LoadingPlugin)
     .use(VueAxios, axios)
     .use(router)
     .use(vuetify)
     .component('qr-code', VueQRCodeComponent)
     .mount('#app')
+
